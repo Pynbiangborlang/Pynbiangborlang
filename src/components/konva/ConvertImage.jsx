7675bbe4a5e98c604e21e4df42da06b46f3034e8
Pynbiangborlang <pynbiangborlang.rapthap@sonicbolt.com>
2022-08-01 21:51:03 +0530

    Refactoring code

--- src/components/konva/ConvertImage.jsx
+++ src/components/konva/ConvertImage.jsx
@@ -4,6 +4,5 @@ import useImage from "use-image";
 
 export const ConvertImage = ({ x, y, w, h, src }) => {
   const [image] = useImage(src);
-  debugger;
   return <Image x={x} y={y} width={w} height={h} image={image} />;
 };
--- src/lib/PynbiangReactKonva/PolygonConstructor.jsx
+++ src/lib/PynbiangReactKonva/PolygonConstructor.jsx
@@ -30,6 +30,7 @@ export const PolygonConstructor = ({ scale, callBack, isMultiple, isEditing }) =
     });
     setnextPoint([]);
   }
+    
   };
   
   const handleClick = (e) => {
